dc659304a124b02260490ee6249d251dbb4a27e2
aenonod <abegail.nonod13@gmail.com>
2025-03-27 21:15:40 +0800

    add initial code to print program without using lstrip

--- program_without_using_lstrip.py
+++ program_without_using_lstrip.py
@@ -1,2 +1,10 @@
 # Ask for user input with leading whitespaces
+user_input = input("Input your full name: ")
+
 # Remove spaces without using .lstrip()
+while True:
+    try:
+        output = user_input.replace(" ", "")
+        print(output)
+    except ValueError:
+        break
