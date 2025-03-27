9093ec9084ff7f3ceae275e8b524ae9ed7a2aee7
aenonod <abegail.nonod13@gmail.com>
2025-03-27 23:23:08 +0800

    add program code to print without using lower

--- program_without_using_lower.py
+++ program_without_using_lower.py
@@ -1,3 +1,12 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Convert text into lowercase without using .lower()
-# For loop to check each char
+lowercase = ""
+for char in user_input:     # For loop to check each char
+    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        new_char = ord(char) + 32     # Use ASCII to convert upper to lower
+        lowercase += chr(new_char)
+    else:
+        lowercase += char
+print(lowercase)
