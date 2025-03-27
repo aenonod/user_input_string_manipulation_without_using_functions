46256e7f3b0cad75e19f5c468ba91fb48348d5da
aenonod <abegail.nonod13@gmail.com>
2025-03-27 22:22:50 +0800

    add initial code to print program without using removeprefix

--- program_without_using_removeprefix.py
+++ program_without_using_removeprefix.py
@@ -1,3 +1,9 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Ask what to remove from the input
+to_remove = input("What do you want to remove from the statement: ")
+
 # Remove prefix without using .removeprefix()
+output = user_input.replace(to_remove,"")
+print(output)
