9951c32dcb9958ea430d5b7e949463856781845d
aenonod <abegail.nonod13@gmail.com>
2025-03-28 23:47:08 +0800

    add initial code to print program without using isupper

--- program_without_using_isupper.py
+++ program_without_using_isupper.py
@@ -1,3 +1,9 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Check is user input is uppercase without using .isupper()
-# Use for loop to check each char
+for char in user_input:     # Use for loop to check each char
+    if char not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        print("False")
+    else:
+        print("True")
