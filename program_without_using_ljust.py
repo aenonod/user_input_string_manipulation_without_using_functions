eaa0b3e9c1d1a0c3365e42cbfac591ee4fed0009
aenonod <abegail.nonod13@gmail.com>
2025-03-29 01:51:42 +0800

    add initial code to print program without using ljust

--- program_without_using_ljust.py
+++ program_without_using_ljust.py
@@ -1,3 +1,15 @@
 # Ask user input
+user_input = input("Input your statement: ")
+
 # Ask for width of string
+width = int(input("Input length of the string: "))
+
 # Add spaces to fill specified width without using .ljust()
+while True:
+    len_str = len(user_input))
+
+    if len_str < width:
+        user_input += " "
+    else:
+        break
+print(user_input)
