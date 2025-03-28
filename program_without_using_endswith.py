f900954325bd95073fa35dc147e1f5173e3f8609
aenonod <abegail.nonod13@gmail.com>
2025-03-29 00:37:32 +0800

    add program code to print program without using endswith

--- program_without_using_endswith.py
+++ program_without_using_endswith.py
@@ -1,3 +1,14 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Ask if the user_input ends with the given word
+suffix = input("Input the suffix to check: ")
+
 # Check if it ends with the given word without using .endswith()
+len_of_suffix = len(suffix)
+from_string = user_input[-(len_of_suffix):]
+
+if suffix == from_string:
+    print(True)
+else:
+    print(False)
