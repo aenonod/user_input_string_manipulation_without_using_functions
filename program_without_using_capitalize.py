093404418d9f1ae7c8243ee9f269b6eeacee7b2d
aenonod <abegail.nonod13@gmail.com>
2025-03-29 20:47:57 +0800

    fix: issue with output by breaking the codes into parts

--- program_without_using_capitalize.py
+++ program_without_using_capitalize.py
@@ -4,14 +4,16 @@ user_input = input("Input your statement: ")
 # Capitalize the first letter without using .capitalize()
 # Use ASCII code to convert lower to upper and vice versa
 capitalize = ""
-for char in user_input:     # For loop to check each char
-    if user_input[0] in "abcdefghijklmnopqrstuvwxyz":
-        new_char = ord(user_input[0]) - 32
-        capitalize += chr(new_char) + user_input[1:]
+if user_input[0] in "abcdefghijklmnopqrstuvwxyz":
+    new_char = ord(user_input[0]) - 32
+    capitalize += chr(new_char)
+else:
+    capitalize += user_input[0]
 
-    if user_input[1:] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        new_char = ord(user_input[1:]) + 32
-        capitalize += new_char
+for char in user_input[1:]:     # For loop to check each char
+    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        new_char = ord(char) + 32
+        capitalize += chr(new_char)
     else:
         capitalize += char
 print(capitalize)
