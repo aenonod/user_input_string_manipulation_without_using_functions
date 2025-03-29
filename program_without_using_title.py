27505034da632e732547d4795dcbc90b00bc311e
aenonod <abegail.nonod13@gmail.com>
2025-03-29 22:19:49 +0800

    feat: add if-else inside for loop to process each word individually

--- program_without_using_title.py
+++ program_without_using_title.py
@@ -8,3 +8,12 @@ for word in user_input.split():
     if word[0] in "abcdefghijklmnopqrstuvwxyz":
         new_charac = ord(word[0]) - 32
         titled += chr(new_charac)
+    else:
+        titled += word
+
+    if word[1:] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        new_charac = ord(word[1:]) + 32
+        titled += chr(new_charac)
+    else:
+        titled += word
+print(titled)
