a1e3b2d20be0ecd2de6293d05f465cba6b25c98a
aenonod <abegail.nonod13@gmail.com>
2025-03-29 20:31:49 +0800

    add initial code to print program without using capitalize

--- program_without_using_capitalize.py
+++ program_without_using_capitalize.py
@@ -1,3 +1,17 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Capitalize the first letter without using .capitalize()
-# Try to utilize if-else statement
+# For loop to check each char
+# Use ASCII code to convert lower to upper and vice versa
+capitalize = ""
+for char in user_input:
+    if user_input[0] in "abcdefghijklmnopqrstuvwxyz":
+        new_char = ord(user_input[0]) - 32
+        capitalize += chr(new_char)
+    elif user_input[1:] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        new_char = ord(user_input[1:]) + 32
+        capitalize += new_char
+    else:
+        capitalize += char
+print(capitalize)
