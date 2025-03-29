be650ec7b10e9abcc399b96a97df91dffc220ffe
aenonod <abegail.nonod13@gmail.com>
2025-03-29 23:12:50 +0800

    refactor: change display of if-condition

--- program_without_using_capitalize.py
+++ program_without_using_capitalize.py
@@ -4,14 +4,14 @@ user_input = input("Input your statement: ")
 # Capitalize the first letter without using .capitalize()
 # Use ASCII code to convert lower to upper and vice versa
 capitalize = ""
-if user_input[0] in "abcdefghijklmnopqrstuvwxyz":
+if "a" <= user_input[0] <= "z":
     new_char = ord(user_input[0]) - 32
     capitalize += chr(new_char)
 else:
     capitalize += user_input[0]
 
 for char in user_input[1:]:     # For loop to check each char
-    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    if "A" <= char <= "Z":
         new_char = ord(char) + 32
         capitalize += chr(new_char)
     else:
