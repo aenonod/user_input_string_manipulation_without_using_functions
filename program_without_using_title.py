3e134d052d69dd8528dfd1f7fb54e5e513302fc7
aenonod <abegail.nonod13@gmail.com>
2025-03-29 22:01:22 +0800

    add initial code to print program without using title

--- program_without_using_title.py
+++ program_without_using_title.py
@@ -1,3 +1,10 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Convert text to title casing without using .title()
 # Split input by words
+titled = ""
+for word in user_input.split():
+    if word[0] in "abcdefghijklmnopqrstuvwxyz":
+        new_charac = ord(word[0]) - 32
+        titled += chr(new_charac)
