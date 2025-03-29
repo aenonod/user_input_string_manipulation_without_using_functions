5044a72779936f43e11166f3f748965af9b0a917
aenonod <abegail.nonod13@gmail.com>
2025-03-29 19:42:33 +0800

    fix: issue with output display by adding elif specifically for lowercase

--- program_without_using_swapcase.py
+++ program_without_using_swapcase.py
@@ -7,7 +7,9 @@ for char in user_input:     # For loop to check each char
     if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
         new_char = ord(char) + 32     # Use ASCII code for upper to lower
         swapped += chr(new_char)
-    else:
+    elif char in "abcdefghijklmnopqrstuvwxyz":
         new_char = ord(char) - 32     # Use ASCII code for lower to upper
         swapped += chr(new_char)
+    else:
+        swapped += char
 print(swapped)
