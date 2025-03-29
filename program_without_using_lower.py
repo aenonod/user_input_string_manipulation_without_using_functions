53f95ec52f7ec90bd0162a4a788cb616e6e5769d
aenonod <abegail.nonod13@gmail.com>
2025-03-29 23:18:48 +0800

    refactor: change display of if-condition

--- program_without_using_lower.py
+++ program_without_using_lower.py
@@ -4,7 +4,7 @@ user_input = input("Input your statement: ")
 # Convert text into lowercase without using .lower()
 lowercase = ""
 for char in user_input:     # For loop to check each char
-    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    if "A" <= char <= "Z":
         new_char = ord(char) + 32     # Use ASCII to convert upper to lower
         lowercase += chr(new_char)
     else:
