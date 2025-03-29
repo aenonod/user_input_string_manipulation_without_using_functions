8d8b847a45d02d41bc1cb0c101a6b103c35db812
aenonod <abegail.nonod13@gmail.com>
2025-03-29 23:17:35 +0800

    refactor: change display of if-condition

--- program_without_using_isupper.py
+++ program_without_using_isupper.py
@@ -4,7 +4,7 @@ user_input = input("Input your statement: ")
 # Check is user input is uppercase without using .isupper()
 output = ""
 for char in user_input:     # Use for loop to check each char
-    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    if "A" <= char <= "Z":
         output = True
     else:
         output = False
