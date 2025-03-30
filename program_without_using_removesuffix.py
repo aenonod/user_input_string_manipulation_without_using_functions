048d0c517d5b7b9d3c67466b4af56ba062f3f50c
aenonod <abegail.nonod13@gmail.com>
2025-03-30 22:15:38 +0800

    add program code to print program without using removesuffix

--- program_without_using_removesuffix.py
+++ program_without_using_removesuffix.py
@@ -1,3 +1,12 @@
-# Ask for user input
+# Ask for user 
+user_input = input("Input your statement: ")
+
 # Ask what to remove from the input
+suffix_to_remove = input("What do you want to remove from the statement: ")
+
 # Remove suffix without using .removesuffix()
+if user_input.endswith(suffix_to_remove):
+    output = user_input.replace(suffix_to_remove,"")
+    print(output)
+else:
+    print(user_input)
