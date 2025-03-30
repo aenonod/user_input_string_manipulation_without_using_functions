c9ff87dc4b2fee182690e97f87d3f4f20ff2293a
aenonod <abegail.nonod13@gmail.com>
2025-03-30 21:28:16 +0800

    add initial code to print program without using rstrip

--- program_without_using_rstrip.py
+++ program_without_using_rstrip.py
@@ -1,2 +1,8 @@
 # Ask for user input with trailing whitespaces
+user_input = input("Input your statement: ")
+
 # Remove spaces without using .rstrip()
+output = user_input
+while output and output[-1] == "*":
+    output = output[-2:]
+print(output)
