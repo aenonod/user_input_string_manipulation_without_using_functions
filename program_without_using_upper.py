935ddc87b2abf4db6265a29a57cd47028b06f8a6
aenonod <abegail.nonod13@gmail.com>
2025-03-30 22:55:58 +0800

    add initial code to print program without using upper

--- program_without_using_upper.py
+++ program_without_using_upper.py
@@ -1,4 +1,12 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Convert text into uppercase without using .upper()
-# For loop to check each char
-# Use ASCII to convert lower to upper
+uppercase = ""
+for char in user_input:     # For loop to check each char
+    if "a" <= char <= "z":
+        new_char = ord(char) - 32     # Use ASCII to convert lower to upper
+        new_char += uppercase
+    else:
+        uppercase += char
+print(uppercase)
