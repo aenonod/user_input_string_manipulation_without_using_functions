2b409c313a02af2a044d936b958998f988432e86
aenonod <abegail.nonod13@gmail.com>
2025-03-30 23:00:08 +0800

    fix: issue with unsupported operand types for +=

--- program_without_using_upper.py
+++ program_without_using_upper.py
@@ -6,7 +6,7 @@ uppercase = ""
 for char in user_input:     # For loop to check each char
     if "a" <= char <= "z":
         new_char = ord(char) - 32     # Use ASCII to convert lower to upper
-        new_char += uppercase
+        uppercase += chr(new_char)
     else:
         uppercase += char
 print(uppercase)
