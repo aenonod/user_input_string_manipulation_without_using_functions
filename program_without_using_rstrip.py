93c3df8a8c765f72d7751485894cd74b5a77ff77
aenonod <abegail.nonod13@gmail.com>
2025-03-30 21:34:35 +0800

    fix: issue with while loop

--- program_without_using_rstrip.py
+++ program_without_using_rstrip.py
@@ -3,6 +3,6 @@ user_input = input("Input your statement: ")
 
 # Remove spaces without using .rstrip()
 output = user_input
-while output and output[-1] == "*":
-    output = output[-2:]
+while output and output[-1] == " ":
+    output = output[:-1]
 print(output)
