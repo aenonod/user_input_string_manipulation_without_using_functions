6c17e709325ba456a9bf4aa9e72f7bf75966fa3e
aenonod <abegail.nonod13@gmail.com>
2025-03-31 22:30:25 +0800

    add initial code to print program without using count

--- program_without_using_count.py
+++ program_without_using_count.py
@@ -1,3 +1,17 @@
 # Ask for user input
+user_input = input("Input text/number: ")
+
 # Ask for a string to count
+word_to_count = input("Input text/number to count: ")
+
 # Count the occurences of the specified string without using .count()
+count = 0
+position = 0
+while True:
+    position = user_input.find(word_to_count)
+    
+    if position == -1:
+        break
+    count += 1
+    position += 1
+print(count)
