bb5fb85ed7d1b9718f8f8d00e71555e45b512618
aenonod <abegail.nonod13@gmail.com>
2025-03-31 22:37:12 +0800

    feat: add starting and ending indices

--- program_without_using_count.py
+++ program_without_using_count.py
@@ -4,11 +4,15 @@ user_input = input("Input text/number: ")
 # Ask for a string to count
 word_to_count = input("Input text/number to count: ")
 
+# Ask where to start and end the counting process
+start = input("Input index where to start: ")
+end = input("Input index where to end: ")
+
 # Count the occurences of the specified string without using .count()
 count = 0
 position = 0
 while True:
-    position = user_input.find(word_to_count)
+    position = user_input.find(word_to_count, start, end)
     
     if position == -1:
         break
