77dbbd5e1efeca711837e17c6d647c9b5a0c75db
aenonod <abegail.nonod13@gmail.com>
2025-03-31 19:57:02 +0800

    fix: if-else condition of islower for accurate results

--- program_without_using_islower.py
+++ program_without_using_islower.py
@@ -2,10 +2,9 @@
 user_input = input("Input your statement: ")
 
 # Check if user input is lowercase without using .islower()
-output = ""
+output = True
 for char in user_input:     # Use for loop to check each char
-    if "a" <= char <= "z":
-        output = True
-    else:
+    if not "a" <= char <= "z":
         output = False
+        break
 print(output)
