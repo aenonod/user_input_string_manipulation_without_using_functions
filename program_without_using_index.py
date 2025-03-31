49298d003f5c8cb907226665996d9c071920c9b3
aenonod <abegail.nonod13@gmail.com>
2025-03-31 23:03:28 +0800

    add program code to print without using index

--- program_without_using_index.py
+++ program_without_using_index.py
@@ -1,3 +1,17 @@
 # Ask for user input
+user_input = input("Input text/number: ")
+
 # Ask for a string to find
+value = input("Input substring: ")
+
 # Return the index without using .index()
+position = 0
+while True:
+    try:
+        position = user_input.find(value, position, len(user_input))
+
+        if position == -1:
+            break
+        output = print(f"Substring found at the index: {position}")
+    except ValueError:
+        output = "Substring not found."
