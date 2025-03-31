7d4f3353c1b6472a99f4d03903dac1410e94925b
aenonod <abegail.nonod13@gmail.com>
2025-03-31 20:32:09 +0800

    add program code to print without using startswith

--- program_without_using_startswith.py
+++ program_without_using_startswith.py
@@ -1,3 +1,14 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Ask if the user_input starts with the given word
+prefix = input("Input the prefix to check: ")
+
 # Check if it starts with the given word without using .startswith()
+len_of_prefix = len(prefix)
+from_string = user_input[:(len_of_prefix)]
+                         
+if prefix == from_string:
+    print(True)
+else:
+    print(False)
