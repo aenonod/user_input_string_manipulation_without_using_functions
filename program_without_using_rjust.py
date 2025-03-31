cefa928e2c1381af8b69651ee9d45a743cb4169f
aenonod <abegail.nonod13@gmail.com>
2025-03-31 21:12:21 +0800

    add program code to print without using rjust

--- program_without_using_rjust.py
+++ program_without_using_rjust.py
@@ -1,3 +1,10 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Ask for width of string
+width = int(input("Input length of the string: "))
+
 # Add spaces to fill specified width without using .rjust()
+while len(user_input) < width:
+    user_input = " " + user_input
+print(user_input)
