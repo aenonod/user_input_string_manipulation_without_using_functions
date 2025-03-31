a4b41193eab268bbc4e971920c4fc2351a0ed527
aenonod <abegail.nonod13@gmail.com>
2025-03-31 22:04:37 +0800

    add initial code to print program without using zfill

--- program_without_using_zfill.py
+++ program_without_using_zfill.py
@@ -1,3 +1,10 @@
 # Ask for user input
+user_input = input("Input text/number: ")
+
 # Ask for width of string
+width = int(input("Input length of the string: "))
+
 # Add zeros in front of the user_input without using .zfill()
+while len(user_input) < width:
+    output = "0" + user_input
+print(output)
