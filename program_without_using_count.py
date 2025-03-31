9dead3cb7afdc869dcc3799d57efc19631cb09c7
aenonod <abegail.nonod13@gmail.com>
2025-03-31 22:47:48 +0800

    refactor: change indices input into default

--- program_without_using_count.py
+++ program_without_using_count.py
@@ -4,15 +4,11 @@ user_input = input("Input text/number: ")
 # Ask for a string to count
 word_to_count = input("Input text/number to count: ")
 
-# Ask where to start and end the counting process
-start = int(input("Index where to start: "))
-end = int(input("Index where to end: "))
-
 # Count the occurences of the specified string without using .count()
 count = 0
-position = start
+position = 0
 while True:
-    position = user_input.find(word_to_count, position, end)
+    position = user_input.find(word_to_count, position, len(user_input))
     
     if position == -1:
         break
