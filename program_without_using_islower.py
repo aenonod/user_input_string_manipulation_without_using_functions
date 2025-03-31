cb67e49559fd374f1df2e59278c400e7051ba9cc
aenonod <abegail.nonod13@gmail.com>
2025-03-31 19:52:23 +0800

    add initial code to print program without using islower

--- program_without_using_islower.py
+++ program_without_using_islower.py
@@ -1,3 +1,11 @@
 # Ask for user input
+user_input = input("Input your statement: ")
+
 # Check if user input is lowercase without using .islower()
-# Use for loop to check each char
+output = ""
+for char in user_input:     # Use for loop to check each char
+    if "a" <= char <= "z":
+        output = True
+    else:
+        output = False
+print(output)
