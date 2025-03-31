7b5460ba3156113b75eea93b036daed0b3692099
aenonod <abegail.nonod13@gmail.com>
2025-03-31 23:35:47 +0800

    add program code to print without using rindex

--- program_without_using_rindex.py
+++ program_without_using_rindex.py
@@ -1,3 +1,16 @@
 # Ask for user input
+user_input = input("Input text/number: ")
+
 # Ask for a string to find
+value = input("Input substring: ")
+
 # Return the index without using .rindex()
+position = len(user_input)
+while True:
+        position = user_input.find(value, 0, position)
+
+        if position == -1:
+            print("Substring not found.")
+            break
+        output = print(f"Substring found at the index: {position}")
+        break
